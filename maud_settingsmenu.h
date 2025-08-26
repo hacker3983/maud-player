3592155dd90bae4de749d8a242cd9884181a0f42
hacker3983 <mrgamer19554@gmail.com>
2025-08-26 15:53:04 -0400

    Added add folder button to music library tab and refactored source files

--- maud_settingsmenu.h
+++ maud_settingsmenu.h
@@ -16,13 +16,25 @@ void maud_settingmenu_createnavbar(maud_t* maud, SDL_Color color, SDL_Color cust
     SDL_Color musiclibrary_tabcolor);
 void maud_settingmenu_rendernavbar(maud_t* maud);
 void maud_settingmenu_rendertab_underline(maud_t* maud, SDL_Color underline_color);
-void maud_settingmenu_displaycustomize_tab(maud_t* maud);
-void maud_settingmenu_displaymusiclibrary_tab(maud_t* maud);
-void maud_settingmenu_displayabout_tab(maud_t* maud);
-void maud_settingmenu_displaycurrent_tab(maud_t* maud);
 void maud_settingmenu_handlebackbutton(maud_t* maud);
 void maud_settingmenu_handlecustomize_tab(maud_t* maud);
 void maud_settingmenu_handlemusiclibrary_tab(maud_t* maud);
 void maud_settingmenu_handleabout_tab(maud_t* maud);
+void maud_settingmenu_rendercustomizetab_headertext(maud_t* maud,
+    maud_customizetab_t* customize_tab);
+void maud_settingmenu_rendercustomizetab_colorpicker(maud_t* maud,
+    maud_customizetab_t* customize_tab);
+void maud_settingmenu_rendermusiclibrary_headertext(maud_t* maud,
+    maud_musiclibrarytab_t* musiclibrary_tab);
+void maud_settingmenu_rendermusiclibrary_locationsection(maud_t* maud,
+    maud_musiclibrarytab_t* musiclibrary_tab);
+void maud_settingmenu_rendermusiclibrary_addlocationbtn(maud_t* maud,
+    maud_musiclibrarytab_t* musiclibrary_tab);
+void maud_settingmenu_rendermusiclibrary_locations(maud_t* maud,
+    maud_musiclibrarytab_t* musiclibrary_tab);
+void maud_settingmenu_displaycustomize_tab(maud_t* maud);
+void maud_settingmenu_displaymusiclibrary_tab(maud_t* maud);
+void maud_settingmenu_displayabout_tab(maud_t* maud);
+void maud_settingmenu_displaycurrent_tab(maud_t* maud);
 void maud_settingmenu_destroy(maud_t* maud);
 #endif
